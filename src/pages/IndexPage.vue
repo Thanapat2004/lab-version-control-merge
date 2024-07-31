1f211e7c53acff656ff8443607f58ca4b997c48f
Thanapat2004 <Sack.thanapat0157@gmail.com>
2024-07-31 11:48:26 +0700

    modified language variable and input language

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -35,7 +35,16 @@
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type your surname']"
       />
-
+      <!--language -->
+      <q-input
+        filled
+        v-model="language"
+        label="ภาษา *"
+        hint="ภาษา"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'กรุณาใส่ภาษา']"
+      />
+      <!-- age -->
       <q-input
         filled
         type="number"
@@ -69,6 +78,7 @@ export default {
     const id = ref("1234")
     const name = ref("ธนพัทร")
     const surname = ref(["บุญผัด"])
+    const language =ref("ภาษาไทย")
     const age = ref(null)
     const accept = ref(false)
 
@@ -76,6 +86,7 @@ export default {
       id,
       name,
       surname,
+      language,
       age,
       accept,
 
@@ -102,6 +113,7 @@ export default {
         name.value = null
         age.value = null
         accept.value = false
+        language.value = null
       }
     }
   }
